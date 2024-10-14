2bf5d7de055edb94e6f874b78a0b7550a19ec4d0
Henelio <59258424+henelio-dev@users.noreply.github.com>
2024-10-14 22:38:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ client.db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0
