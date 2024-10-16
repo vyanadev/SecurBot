bb06e544de9ee56a91c8f06776ab54bd862fa700
vyanadev <59258424+vyanadev@users.noreply.github.com>
2024-10-16 22:19:55 +0200

    Update help.js

--- cmd/misc/help.js
+++ cmd/misc/help.js
@@ -2,38 +2,56 @@ const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, 
 
 module.exports = {
     name: 'help',
+    description: "Affiche la pages d'aide du bot",
+    usage: `[commandes]`,
     aliases: ["h"],
+    cooldowns: 5,
     execute: async (client, message, args, prefix, color) => {
         try {
-            const [ownerResult] = await client.db.promise().query('SELECT * FROM bot_owners WHERE user_id = ?', [message.author.id]);
-            if (ownerResult.length === 0) return;
+            // Vérifier si l'utilisateur est un propriétaire du bot
+            const [ownerResult] = await client.db.promise().query('SELECT COUNT(*) AS count FROM bot_owners WHERE user_id = ?', [message.author.id]);
+            if (ownerResult[0].count === 0) return;
+
+            // Si un argument est fourni, afficher l'aide pour cette commande spécifique
+            if (args.length > 0) {
+                const commandName = args[0].toLowerCase();
+                const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+                if (!command) {
+                    return message.reply(`La commande \`${commandName}\` n'existe pas.`);
+                }
+
+                const commandHelp = new EmbedBuilder()
+                    .setTitle(`Aide pour la commande: \`${command.name}\``)
+                    .setColor(color)
+                    .setFooter({ text: process.env.BOT_FOOTER });
+
+                if (command.description) commandHelp.setDescription(`\`${command.description}\``);
+                if (command.aliases) commandHelp.addFields({ name: 'Aliases', value: `\`${command.aliases.join('`, `')}\`` });
+                if (command.usage) commandHelp.addFields({ name: 'Utilisation', value: `\`${prefix}${command.name} ${command.usage}\`` });
+                if (command.cooldowns) commandHelp.addFields({ name: 'Cooldown', value: `\`${command.cooldowns} secondes\`` });
+                if (command.examples) commandHelp.addFields({ name: 'Exemples', value: command.examples.map(example => `\`${prefix}${command.name} ${example}\``).join('\n') });
+
+                return message.reply({ embeds: [commandHelp], allowedMentions: { repliedUser: false } });
+            }
 
             const embed = new EmbedBuilder()
-                .setTitle(`Help`)
+                .setTitle(`Aide du Bot`)
                 .setColor(color)
                 .setFooter({ text: process.env.BOT_FOOTER })
-                .setDescription("Sélectionnez une catégorie dans le menu déroulant ci-dessous pour voir les commandes correspondantes.");
+                .setDescription(`Sélectionnez une catégorie dans le menu déroulant ci-dessous pour voir les commandes correspondantes.\nPour obtenir de l'aide sur une commande spécifique, utilisez \`${prefix}help <nom de la commande>\`.`);
+
+            const categories = [
+                { label: 'Antiraid', description: 'Commandes liées à l\'antiraid', value: 'antiraid', emoji: '🛡️' },
+                { label: 'Modération', description: 'Commandes de modération', value: 'moderation', emoji: '🛠️' },
+                { label: 'Misc', description: 'Commandes diverses', value: 'misc', emoji: '📌' },
+                { label: 'Owner', description: 'Commandes réservées aux propriétaires', value: 'owner', emoji: '👑' }
+            ];
 
             const selectMenu = new StringSelectMenuBuilder()
                 .setCustomId('help_category')
                 .setPlaceholder('Choisissez une catégorie')
-                .addOptions([
-                    {
-                        label: 'Antiraid',
-                        description: 'Commandes liées à l\'antiraid',
-                        value: 'antiraid',
-                    },
-                    {
-                        label: 'Misc',
-                        description: 'Commandes diverses',
-                        value: 'misc',
-                    },
-                    {
-                        label: 'Owner',
-                        description: 'Commandes réservées aux propriétaires',
-                        value: 'owner',
-                    },
-                ]);
+                .addOptions(categories);
 
             const linkButton = new ButtonBuilder()
                 .setLabel('Support Discord')
@@ -57,24 +75,55 @@ module.exports = {
                 }
 
                 if (interaction.isStringSelectMenu()) {
-                    let description;
-                    switch (interaction.values[0]) {
-                        case 'antiraid':
-                            description = `\`\`\`${prefix}antiraid [on/off/max/config]\n${prefix}whitelist <add/clear/list/remove>\n${prefix}verify <on/off>\`\`\``;
-                            break;
-                        case 'misc':
-                            description = `\`\`\`${prefix}help\n${prefix}ping\`\`\``;
-                            break;
-                        case 'owner':
-                            description = `\`\`\`${prefix}blacklist <add/clear/list/remove>\n${prefix}owner <add/clear/list/remove>\`\`\``;
-                            break;
-                    }
+                    const category = interaction.values[0];
+                    const commands = {
+                        antiraid: `
+\`${prefix}antiraid [on/off/max/config]\` - Configure les paramètres généraux de l'antiraid
+\`${prefix}whitelist <add/clear/list/remove>\` - Gère la liste blanche de l'antiraid
+\`${prefix}antibot [on/off]\` - Active ou désactive la protection contre l'ajout de bots
+\`${prefix}antichannel [on/off]\` - Active ou désactive la protection contre la création/suppression de salons
+\`${prefix}antiwebhook [on/off]\` - Active ou désactive la protection contre la création de webhooks
+\`${prefix}antieveryone [on/off]\` - Active ou désactive la protection contre les mentions @everyone
+\`${prefix}antiban [on/off]\` - Active ou désactive la protection contre les bannissements en masse
+
+Pour plus de détails sur une commande, utilisez \`${prefix}help <nom de la commande>\`
+`,
+moderation: `
+\`${prefix}ban <@utilisateur> [raison]\` - Bannir un utilisateur du serveur
+\`${prefix}kick <@utilisateur> [raison]\` - Expulser un utilisateur du serveur
+\`${prefix}mute <@utilisateur> <durée> [raison]\` - Mettre un utilisateur en sourdine
+\`${prefix}clear <nombre>\` - Supprimer un certain nombre de messages
+\`${prefix}warn <@utilisateur> <raison>\` - Avertir un utilisateur
+\`${prefix}unwarn <@utilisateur> <ID avertissement>\` - Retirer un avertissement
+\`${prefix}warnlist [@utilisateur]\` - Afficher la liste des avertissements d'un utilisateur
+\`${prefix}lock [on/off]\` - Verrouiller ou déverrouiller un canal
+
+Pour plus de détails sur une commande, utilisez \`${prefix}help <nom de la commande>\`
+`,
+                        misc: `
+\`${prefix}help\` - Affiche ce message d'aide
+\`${prefix}ping\` - Vérifie la latence du bot
+\`${prefix}serverinfo\` - Affiche les informations du serveur
+\`${prefix}botinfo\` - Affiche les informations du bot
+\`${prefix}userinfo [@utilisateur]\` - Affiche les informations d'un utilisateur
+\`${prefix}banner [@utilisateur]\` - Affiche la bannière d'un utilisateur
+\`${prefix}avatar [@utilisateur]\` - Affiche l'avatar d'un utilisateur
+
+Pour plus de détails sur une commande, utilisez \`${prefix}help <nom de la commande>\`
+`,
+                        owner: `
+\`${prefix}blacklist <add/clear/list/remove>\` - Gère la liste noire du bot
+\`${prefix}owner <add/clear/list/remove>\` - Gère les propriétaires du bot
+
+Pour plus de détails sur une commande, utilisez \`${prefix}help <nom de la commande>\`
+`
+                    };
 
                     const newEmbed = new EmbedBuilder()
-                        .setTitle(`Help - ${interaction.values[0].charAt(0).toUpperCase() + interaction.values[0].slice(1)}`)
+                        .setTitle(`Aide - ${category.charAt(0).toUpperCase() + category.slice(1)}`)
                         .setColor(process.env.DEFAULT_COLOR)
-                        .setFooter({ text: process.env.BOT_FOOTER  })
-                        .setDescription(description);
+                        .setFooter({ text: process.env.BOT_FOOTER })
+                        .setDescription(commands[category]);
 
                     await interaction.update({ embeds: [newEmbed] });
                 }
@@ -89,4 +138,4 @@ module.exports = {
             message.reply("Une erreur s'est produite lors de l'exécution de la commande.").catch(console.error);
         }
     }
-};  
+};
